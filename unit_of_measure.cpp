c170f500d3b280d9f5b5f98ec3564016bfebf7cc
Valentin Nkana <valentin.nkana@intermaths.eu>
2020-10-15 22:37:04 +0200

    exercices on the measure

---
+++ unit_of_measure.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+double number;
+string si_unit;
+int main(){
+cin >> number;
+cin >> si_unit;
+if (si_unit == "inch" or si_unit == "m")
+{
+    cout<<number<<" "<< si_unit << endl;
+}
+
+    return 0;
+}
